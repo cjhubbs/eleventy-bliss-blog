9ad00ee02f88d3a1767ccd3a1b2187675ef87072
Chris Hubbs <chris.hubbs@collins.com>
2023-11-07 09:57:55 -0600

    basic config

--- content/_data/siteConfig.js
+++ content/_data/siteConfig.js
@@ -9,16 +9,16 @@ module.exports = {
     // OpenGraph tags and feeds.
     // Can also be used anywhere on the site as a Nunjucks variable:
     // {{ siteConfig.site.logo }}
-    title: 'Bliss',
+    title: 'ChrisHubbs.com',
     // Site description. Used in many places, including meta title,
     // OpenGraph tags and feeds.
     // It is also used as a fallback description if no custom description
     // is provided for a page / post.
-    description: 'Simple Eleventy blog template',
+    description: "One man's thoughts for almost 20 years...",
     // Production URL. Used by 'alwaysProductionUrl' filter.
     // If none is provided, relative paths will be used
     // and in some cases (like sitemap.xml) that is TOTALLY NOT what you want
-    url: process.env.URL || 'https://eleventy-bliss.vercel.app/',
+    url: process.env.URL || 'https://beta.chrishubbs.com/',
     // Image used as a site logo. If none is provided, site title as text
     // will be displayed as fallback.
     // If you opt for text fallback, you may want to adjust
@@ -32,7 +32,7 @@ module.exports = {
     // with {% currentYear %} expression
     // so that you don't have to update the copyright year
     // in your footer by hand ever again :)
-    startYear: 2022,
+    startYear: 2004,
     // Generator information to display in <html> tag and the site footer.
     // Absolutely optional, but it will make Zach Leatherman happy :)
     // Also, it makes you eligible to be featured on Speedlify:
@@ -68,10 +68,10 @@ module.exports = {
   author: {
     // Your name. Self-explanatory I guess?
     // And you can use it anywhere on the site as {{ siteConfig.author.name }}
-    name: 'John Doe',
+    name: 'Chris Hubbs',
     // Your website URL. Used in the footer. Optional.
     // Doesn't have to be the same link as this site
-    url: 'https://eleventy-bliss.vercel.app/',
+    url: 'https://beta.chrishubbs.com/',
     // Your fediverse links (e.g. Mastodon). You can provide more than one.
     // For the first account in this array your site will generate
     // .well-known/webfinger file for you so that you'll be searchable
@@ -81,9 +81,9 @@ module.exports = {
     // on Mastodon and other services that use this method of verification.
     fediverse: [
       {
-        username: 'JohnMastodon',
-        server: 'mastodon.social',
-        url: 'https://mastodon.social/@JohnMastodon',
+        username: 'cjhubbs',
+        server: 'mastodon.online',
+        url: 'https://mastodon.online/@cjhubbs',
       },
     ],
   },
@@ -92,17 +92,13 @@ module.exports = {
   // ---------------------------------------------------------------------------
   metaPages: [
     {
-      url: 'https://example.org/link-1/',
-      title: 'Example link 1',
+      url: 'https://bsky.app/cjhubbs',
+      title: 'Bluesky',
     },
     {
-      url: 'https://example.org/link-2/',
-      title: 'Example link 2',
-    },
-    {
-      url: 'https://example.org/link-3/',
-      title: 'Example link 3',
-    },
+      url: 'https://github.com/cjhubbs',
+      title: 'Github',
+    }
   ],
   // ---------------------------------------------------------------------------
   // Default settings for OpenGraph functionality (tags and generated images)
@@ -117,7 +113,7 @@ module.exports = {
     // (build the site and find the images in _site/images/share folder)
     // To modify what generated OG images look like
     // edit content/_data/utils/og-image.njk
-    enableImageGeneration: false,
+    enableImageGeneration: true,
     // Background color for auto-generated OpenGraph images
     ogImageBackgroundColor: '#1773cf',
     // Text color for for auto-generated OpenGraph images
@@ -172,11 +168,11 @@ module.exports = {
   // ---------------------------------------------------------------------------
   shareButtons: [
     'mastodon',
-    'twitter',
+  //  'twitter',
     'linkedin',
     'facebook',
-    'hackernews',
-    'clipboard',
+  //  'hackernews',
+  //  'clipboard',
   ],
   // ---------------------------------------------------------------------------
   // Date formats used on the site (mostly somewhere around post contents).
